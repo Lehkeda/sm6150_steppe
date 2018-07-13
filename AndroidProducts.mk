ac2450993e51564ba21c78309a2d56bb1c616be4
spooja <spooja@codeaurora.org>
2018-07-13 16:22:04 +0530

    Initial changes for talos.
    Change-Id: I5467806ec9d856045bcb131f83ca09d841dc97c2

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,2 @@
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/msmsteppe.mk
+	$(LOCAL_DIR)/talos.mk
--- talos.mk
+++ talos.mk
@@ -2,7 +2,7 @@
 BOARD_AVB_ENABLE := true
 
 #target name, shall be used in all makefiles
-MSMSTEPPE = msmsteppe
+MSMSTEPPE = talos
 
 $(call inherit-product, device/qcom/common/common64.mk)
 
@@ -76,8 +76,7 @@ endif #TARGET_ENABLE_QC_AV_ENHANCEMENTS
 PRODUCT_PACKAGES += android.hardware.media.omx@1.0-impl
 
 # Audio configuration file
--include $(TOPDIR)hardware/qcom/audio/configs/$(MSMSTEPPE)/$(MSMSTEPPE).mk
-
+-include $(TOPDIR)hardware/qcom/audio/configs/msmsteppe/msmsteppe.mk
 #Audio DLKM
 AUDIO_DLKM := audio_apr.ko
 AUDIO_DLKM += audio_wglink.ko
