5c7ffda27ffa6867a8a9b8798be4257ae0ba77cd
Ramakant Singh <ramakant@codeaurora.org>
2018-08-29 18:02:11 +0530

    talos: Add display make file.
    Change-Id: I37408568c4c55b88c321e226337901a91b0d8b19

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -131,7 +131,6 @@ BOARD_VENDOR_KERNEL_MODULES := \
 TARGET_USES_ION := true
 TARGET_USES_NEW_ION_API :=true
 TARGET_USES_QCOM_BSP := false
-TARGET_USES_DRM_PP := true
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 androidboot.usbcontroller=a600000.dwc3 firmware_class.path=/vendor/firmware_mnt/image earlycon=msm_geni_serial,0x880000
 
 BOARD_EGL_CFG := device/qcom/$(TARGET_BOARD_PLATFORM)/egl.cfg
@@ -149,8 +148,6 @@ TARGET_USES_UNCOMPRESSED_KERNEL := false
 MAX_EGL_CACHE_KEY_SIZE := 12*1024
 MAX_EGL_CACHE_SIZE := 2048*1024
 
-TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
-MAX_VIRTUAL_DISPLAY_DIMENSION := 4096
 
 BOARD_USES_GENERIC_AUDIO := true
 BOARD_QTI_CAMERA_32BIT_ONLY := true
@@ -159,7 +156,6 @@ TARGET_NO_RPC := true
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 TARGET_INIT_VENDOR_LIB := libinit_msm
 
-NUM_FRAMEBUFFER_SURFACE_BUFFERS := 2
 TARGET_KERNEL_APPEND_DTB := true
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 
@@ -181,10 +177,6 @@ ifeq ($(HOST_OS),linux)
     endif
 endif
 
-TARGET_USES_GRALLOC1 := true
-TARGET_USES_HWC2 := true
-TARGET_USES_QCOM_DISPLAY_BSP := true
-TARGET_USES_COLOR_METADATA := true
 
 # Enable sensor multi HAL
 USE_SENSOR_MULTI_HAL := true
