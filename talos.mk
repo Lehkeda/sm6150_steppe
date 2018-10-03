8667416e3cd632ccf2b2b5bad51b53ff4dc95b57
Vijay Dandiga <vdandiga@codeaurora.org>
2018-10-03 17:54:02 +0530

    talos.mk: Enable IO Cgroups
    Set the property "ro.vendor.iocgrp.config=1"
    Ensures performance of fore-ground application
    doesn't get effected by back-ground applications.
    
    Change-Id: I30e2e9e82578bf5fe0450cceaca09b8abec981d6

--- talos.mk
+++ talos.mk
@@ -241,6 +241,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.qcom.bluetooth.soc=cherokee
 
+#Property to enable IO cgroups
+PRODUCT_PROPERTY_OVERRIDES += ro.vendor.iocgrp.config=1
+
 #Enable full treble flag
 PRODUCT_FULL_TREBLE_OVERRIDE := true
 PRODUCT_VENDOR_MOVE_ENABLED := true
