00fe66c4b60df82ec01b492ef783ba9d88ac2ac3
padarshr <padarshr@codeaurora.org>
2018-12-17 16:41:09 +0530

    Add framework to dynamically pickup product & board definitions.
    This change will add rules to dynamically pickup the product definitions
    from vendor/qcom/defs/product-defs/[system|vendor]/*.mk and the board
    definitions from vendor/qcom/defs/board-defs/[system|vendor]/*.mk
    in an alphanumeric sorted fashion.
    
    Only TARGET_BOARD_PLATFORM (and not lunch_name/TARGET_PRODUCT/BRAND etc) can
    be used within vendor/qcom/defs/[product|board]-defs/[system|vendor]/*.mk
    to detect the board platform that we are building on.
    
    Also, include common mk from device/qcom/qssi.
    
    Change-Id: I6046129cfabe7d3233f3e5e6483e2b45a66e7111

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -204,3 +204,13 @@ endif
 #All vendor APK will be compiled against system_current API set.
 BOARD_SYSTEMSDK_VERSIONS:=28
 BOARD_VNDK_VERSION:= current
+
+
+#################################################################################
+# This is the End of BoardConfig.mk file.
+# Now, Pickup other split Board.mk files:
+#################################################################################
+# TODO: Relocate the system Board.mk files pickup into qssi lunch, once it is up.
+-include vendor/qcom/defs/board-defs/system/*.mk
+-include vendor/qcom/defs/board-defs/vendor/*.mk
+#################################################################################
