0064e53c3e130c6ca06ed28e8bddea7654c6b92b
Rahul Shahare <rshaha@codeaurora.org>
2019-06-20 15:49:16 +0530

    SM6150: Set Header version for bootimage
    Set Header version to 1 for bootimage and Enable
    BOARD_INCLUDE_RECOVERY_DTBO.
    
    Change-Id: I165130b412020a6dee7fb3473766296f21a7cdb7
    Signed-off-by: Rahul Shahare <rshaha@codeaurora.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,6 +41,13 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/qcom/common
 USE_OPENGL_RENDERER := true
 BOARD_USE_LEGACY_UI := true
 
+#Generate DTBO image
+BOARD_KERNEL_SEPARATED_DTBO := true
+
+# Set Header version for bootimage
+BOARD_BOOTIMG_HEADER_VERSION := 1
+BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+
 ifeq ($(ENABLE_AB), true)
 # Defines for enabling A/B builds
 AB_OTA_UPDATER := true
@@ -88,6 +95,10 @@ BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
         BOARD_USES_RECOVERY_AS_BOOT := true
     else
         BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x04000000
+        ifeq ($(BOARD_KERNEL_SEPARATED_DTBO),true)
+            # Enable DTBO for recovery image
+            BOARD_INCLUDE_RECOVERY_DTBO := true
+        endif
     endif
 else
 # Define the Dynamic Partition sizes and groups.
@@ -95,6 +106,10 @@ else
         BOARD_SUPER_PARTITION_SIZE := 12884901888
     else
         BOARD_SUPER_PARTITION_SIZE := 6442450944
+    endif
+    ifeq ($(BOARD_KERNEL_SEPARATED_DTBO),true)
+        # Enable DTBO for recovery image
+        BOARD_INCLUDE_RECOVERY_DTBO := true
     endif
 BOARD_SUPER_PARTITION_GROUPS := qti_dynamic_partitions
 BOARD_QTI_DYNAMIC_PARTITIONS_SIZE := 6438256640
@@ -225,9 +240,6 @@ USE_SENSOR_HAL_VER := 2.0
 
 #Add non-hlos files to ota packages
 ADD_RADIO_FILES := true
-
-#Generate DTBO image
-BOARD_KERNEL_SEPARATED_DTBO := true
 
 #Enable LM
 TARGET_USES_LM := true
